6ba887f65720673c1b00afb25693defe899a9cad
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-09-09 16:57:43 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -144,8 +144,8 @@ GITHUB_REDIRECT_URI = env(
 SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
 
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/ '
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'http://localhost:5173'
+LOGOUT_REDIRECT_URL = 'http://localhost:5173'
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
