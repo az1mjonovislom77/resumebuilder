0358ded1a1c492ef003b66f96435fa6945586e36
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-09-09 12:23:07 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,8 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/ '
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
